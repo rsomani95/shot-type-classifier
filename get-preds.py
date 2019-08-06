c402a252644f008e066aaff338274d24e1a572e2
rsomani95 <rsomani95@gmail.com>
2019-08-06 19:27:45 +0530

    py script for getting preds on a set of imgs

---
+++ get-preds.py
@@ -0,0 +1,71 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='''
+    ======================================================================
+             Predict shot types using a pretrained ResNet-50
+    ======================================================================
+
+     Usage
+    -------
+
+    python get-preds.py
+        --path_base '/home/user/shot-type-classifier'
+        --path_img '/home/user/Desktop/imgs'
+        --path_preds '/home/user/Desktop/imgs/preds'
+    ''', formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument('--path_base', type=str,
+                    help='path to the "shot-type-classifier" directory')
+parser.add_argument('--path_img', type=str,
+                    help='path to where the images are stored')
+parser.add_argument('--path_preds', type=str, default = None,
+                    help="path where you'd like to store the predictions")
+args = parser.parse_args()
+
+path = args.path_base
+
+from setup import *
+
+x = get_model(Path(path))
+
+def save_preds(path_img, path_save=None):
+    print(path_save)
+    os.mkdir(path_save) if not os.path.exists(path_save) else None
+
+    os.chdir(path)
+    files = [f for f in os.listdir(path_img) if f.endswith(('.jpg', '.jpeg', '.png'))]
+
+    for file in files:
+        # open file
+        x = open_image(file)
+
+        # get preds
+        preds_num = learn.predict(x)[2].numpy()
+
+        # form data-frame
+        df = pd.DataFrame(list(zip(data.classes, preds_num )), columns=['shot-type', 'prediction'])
+
+        # reorder data-frame from largest to smallest shot size
+        df['shot-type'].replace({    'Extreme Wide': 'EWS',
+                                             'Long': 'LS',
+                                           'Medium': 'MS',
+                                  'Medium Close-Up': 'MCU',
+                                         'Close-Up': 'CU',
+                                 'Extreme Close-Up': 'ECU'}, inplace=True)
+        df['shot-type'] = pd.Categorical(df['shot-type'], ['EWS', 'LS', 'MS', 'MCU', 'CU', 'ECU'])
+        df = df.sort_values('shot-type').reset_index(drop=True)
+
+        # probability --> percentage
+        df['prediction'] *= 100
+
+
+        # save to disk
+        fname = file.rpartition('.')[0] + '_preds.csv'
+        if path_save is not None:
+            df.to_csv(Path(path_save)/fname, index=False)
+
+        else:
+            df.to_csv(Path(path_img)/fname, index=False)
+
+save_preds(args.path_img, args.path_preds)
