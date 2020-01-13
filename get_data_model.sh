a2cc991869486169d986b18dacd7511f6870fdbd
rsomani95 <rsomani95@gmail.com>
2020-01-13 22:33:45 +0530

    added onnx and mlmodels

--- get_data_model.sh
+++ get_data_model.sh
@@ -5,11 +5,22 @@ mkdir models
 mkdir train
 mkdir valid
 
+## ---- Download Models ----
+
 echo "Downloading .pth model"
 cd models/
 wget -O shot-type-classifier.pth https://www.dropbox.com/s/f9703kbb2l82fsd/stage-3-2.pth?dl=0
+
 echo "Downloading .pkl model (for inference)"
 wget -O shot-type-classifier.pkl https://www.dropbox.com/s/ixt7oxw8j54aorq/shot-type-classifier.pkl?dl=0
+
+echo "Downloading .onnx model"
+wget -O shot-scale-classifier.onnx https://www.dropbox.com/s/o6u7jml6qik9ru5/shot-scale-classifier.onnx?dl=0
+
+echo "Downloading .mlmodel model"
+wget -O shot-scale-classifier.mlmodel https://www.dropbox.com/s/1p6g52168u7kans/shot-scale-classifier.mlmodel?dl=0
+
+## ---- Download Data ----
 
 echo "Downloading dummy training data"
 cd ../train/
